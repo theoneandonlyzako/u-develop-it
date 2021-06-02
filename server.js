7f3bab2d272a0a96d80fc0ef552a189b00599257
Zak Stamps <theoneandonlyzako@gmail.com>
2021-06-02 16:16:02 -0700

    finished creating candidates

--- server.js
+++ server.js
@@ -3,13 +3,30 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const mysql = require('mysql2');
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Mine6463',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
-
+  
 
 
   // Default response for any other request (Not Found)
