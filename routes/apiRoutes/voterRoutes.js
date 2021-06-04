38875a501bcef860b3c0e0430e0f8abae79ab58e
Zak Stamps <theoneandonlyzako@gmail.com>
2021-06-03 19:08:57 -0700

    added votes function to take in votes and display total votes

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -3,6 +3,26 @@ const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
+// Get All votes
+router.get('/votes', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
+    FROM votes
+    LEFT JOIN candidates ON votes.candidate_id = candidates.id
+    LEFT JOIN parties ON candidates.party_id = parties.id
+    GROUP BY candidate_id ORDER BY count DESC;`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Total Votes',
+            data: rows,
+        });
+    });
+});
+
 // Get All voters
 router.get('/voters', (req, res) => {
     const sql = `SELECT * FROM voters ORDER BY last_name`;
